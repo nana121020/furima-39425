37aa57c94b25407412af77bdfb3de8e4dcacbc74
角居ひなの <135084762+nana121020@users.noreply.github.com>
2023-06-26 21:03:28 +0900

    カラムの変更

--- app/models/oder_address.rb
+++ app/models/oder_address.rb
@@ -4,7 +4,7 @@ class OrderAddress
 
   with_options presence: true do
     validates :user_id
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :postal_number, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
   end
   validates :prefecture, numericality: {other_than: 0, message: "can't be blank"}
 
