364efa7de3fa36cc17559973b6f6a72d58e28ce0
角居ひなの <135084762+nana121020@users.noreply.github.com>
2023-07-04 16:32:04 +0900

    アクションの位置変更

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,7 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user! 
   before_action :move_to_index, only: [:index, :create]
   before_action :set_public_key, only: [:index, :create]
-  before_action :authenticate_user! 
-
   def index
     @order_address = OrderAddress.new
   end
